47ef8e0ecc8e0edb8ee97ff9ad3f81e299a5c913
rmarinn <34529290+rmarinn@users.noreply.github.com>
2024-01-17 07:00:08 +0800

    deleted login and signup modals and moved getConnection() to it's own file

--- informatics/database/login_action.php
+++ informatics/database/login_action.php
@@ -1,18 +1,5 @@
 <?php
-
-function getConn()
-{
-    $dbhost = "127.0.0.1";
-    $dbuser = "root";
-    $dbpass = "";
-    $dbname = "informatics_admission";
-    $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } else {
-        return $conn;
-    }
-}
+require 'connection.php';
 
 function submitFormData()
 {
