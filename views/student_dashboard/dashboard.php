fa9b2871792b4f206cf6f07c98f080d9e872be10
rmarinn <34529290+rmarinn@users.noreply.github.com>
2024-01-22 06:27:44 +0800

    updated examination page

--- views/student_dashboard/dashboard.php
+++ views/student_dashboard/dashboard.php
@@ -8,15 +8,18 @@
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/student_dashboard.css">
+    <?php if ($path == "/examination") : ?>
+        <link rel="stylesheet" href="css/exam.css">
+    <?php endif ?>
 </head>
 
-<body style="height: 100vh;">
-    <div class="d-flex h-100">
+<body style="height: 100vh;" class="m-0 p-0">
+    <div class="d-flex h-100 w-100">
         <div class="col-3 h-100">
             <?php require_once "views/student_dashboard/sidebar.php" ?>
         </div>
         <div class="col d-flex flex-column">
-            <main class="container">
+            <main class="container d-flex justify-content-center mt-3 mr-3">
                 <?php
                 switch ($path) {
                     case '/application':
@@ -44,6 +47,9 @@
         </div>
     </div>
     <?php require_once "views/templates/body_scripts.php" ?>
+    <?php if ($path == "/examination") : ?>
+        <script src="js/exam.js"></script>
+    <?php endif ?>
 </body>
 
 </html>
--- views/student_dashboard/examination.php
+++ views/student_dashboard/examination.php
@@ -1,1 +1,28 @@
-<h1>Examination</h1>
+<div class="h-100 w-100">
+    <h1>Examination</h1>
+    <b class="text-dark">Warning:</b><em class="text-dark">you will have to take the test from the start if you leave this page without finishing the exam.</em>
+    <div class="quiz-container w-100 h-100" id="quiz">
+        <div class="quiz-header">
+            <p id="question"> Question </p>
+        </div>
+        <ul class="item">
+            <li>
+                <input type="radio" name="answer" id="a" class="answer">
+                <label for="a" id="a_text"> Answer</label>
+            </li>
+            <li>
+                <input type="radio" name="answer" id="b" class="answer">
+                <label for="b" id="b_text"> Answer</label>
+            </li>
+            <li>
+                <input type="radio" name="answer" id="c" class="answer">
+                <label for="c" id="c_text"> Answer</label>
+            </li>
+            <li>
+                <input type="radio" name="answer" id="d" class="answer">
+                <label for="d" id="d_text"> Answer</label>
+            </li>
+        </ul>
+        <button id="submit">Submit</button>
+    </div>
+</div>
